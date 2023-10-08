b0ab0e9d376524913cc2c3a3bbafb88faa444270
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2023-10-08 10:53:13 +0200

    error page redirect and wild card routers

--- hotelinventoryapp/src/app/app-routing.module.ts
+++ hotelinventoryapp/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 import { RoomsComponent } from './rooms/rooms.component';
 
 const routes: Routes = [
   { path: 'employee', component: EmployeeComponent },
   { path: 'rooms', component: RoomsComponent },
   { path: '', redirectTo: '/rooms', pathMatch: 'full' },
+  { path: '**', component: NotfoundComponent },
 ];
 
 @NgModule({
--- hotelinventoryapp/src/app/app.module.ts
+++ hotelinventoryapp/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 function initFactory(initService: InitService) {
   return () => initService.init();
@@ -34,6 +35,7 @@ function initFactory(initService: InitService) {
     ContainerComponent,
     EmployeeComponent,
     NavbarComponent,
+    NotfoundComponent,
   ],
   imports: [
     BrowserModule,
